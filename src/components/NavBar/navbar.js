51dde7833c96d38949479a9d92e97f6988bf0c10
yuvalzoref <yuval_zoref@hotmail.com>
2023-08-24 18:17:07 +0100

    styled navbar and put logo on there

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,9 +1,22 @@
 import React from 'react'
-import './navbar.css'
+import './navbar.css';
+import logo from '../../assets/name-logo.png';
+import contactImg from '../../assets/email.png';
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
   return (
-    <div>navbar will be here</div>
+    <nav className='navbar'>
+      <img src={logo} alt="logo" className='logo'/>
+      <div className='desktopMenu'>
+        <Link className="desktopMenuListItem">Home</Link>
+        <Link className="desktopMenuListItem">About</Link>
+        <Link className="desktopMenuListItem">Portfolio</Link>
+        <Link className="desktopMenuListItem">Clients</Link>
+      </div>
+      <button className='desktopMenuBtn'>
+        <img src={contactImg} alt="" className="desktopMenuImg" />Contact me</button>
+    </nav>
   )
 }
 
