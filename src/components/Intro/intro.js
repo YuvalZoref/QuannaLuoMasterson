5a7eccd46eab57330503ead9bb84a9068a713c62
yuvalzoref <yuval_zoref@hotmail.com>
2023-09-11 12:00:15 +0100

    some color changes to BTN

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,15 +1,12 @@
 import React from "react";
 import './intro.css';
 import background from '../../assets/q-intro.png';
-import chinese
- from '../../assets/chinese.png';
 
 const Intro = () => {
   return (
     <section id="intro">
       <div className="introContent">
-        <span className="introText"><span className="introName">Quanna美丽的<img className="chinese" alt="#" src={chinese
-        }/>
+        <span className="introText"><span className="introName">Quanna美丽的
         </span><br />Luo Masterson</span>
       </div>
       <img src={background} alt="Profile" className="background" />
